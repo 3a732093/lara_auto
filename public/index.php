fd579e1eb10defe0b3c97e9204fda80856e4965a
M513-T\admin <elaine@ncut.edu.tw>
2020-10-26 15:32:09 +0800

    將log路徑更正

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ echo "<br>";
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('log/your.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('../log/your.log', Logger::WARNING));
 
 // add records to the log
 $log->warning('Foo');
